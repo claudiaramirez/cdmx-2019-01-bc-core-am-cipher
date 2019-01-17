31d31a31c23f1e9ae5b8df131f5b3b68f6845c19
claudiaramirez <claudiaivonne_15@hotmail.com>
2019-01-17 11:13:38 -0600

    sirve encriptar falata pasar test

--- src/cipher.js
+++ src/cipher.js
@@ -1,62 +1,24 @@
 window.cipher = {
   // ...
-  encode: (offset, string) => {
+  encode: (offset, mensaje) => {
+    let offsetPasear = parseInt(offset)
+    let cifrado = ""; 
+    for (let i = 0; i < mensaje.length; i++)
+    {
+      let letra = mensaje[i]
+      let obtenerAscii = letra.charCodeAt();
+      let conversion = (obtenerAscii-65+offsetPasear)%26+65;
+      
+      let resultadoCifrado = String.fromCharCode(conversion);
+      cifrado+=resultadoCifrado;
+    }
 
-    
-  
-    
-    console.log(offset, string);
-    return string
+    //console.log(cifrado);
+    return cifrado 
   }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /*  x - 65 + n ) % 26 + 65*/
-    /* //funcion Cipher
-function cipher(text){
-  //para guardar las letras del nuevo texto
-  var textCipher = "";
-  //recorriendo el texto ingresado
-  for(var i = 0; i<text.length; i++){
-   //averiguando el codigo ASCII de las letras y aplicando la formula
-   var textChar = (text.charCodeAt(i) - 65 + 33)%26 + 65;
-   //convirtiendo el texto cifrado al cod. ASCII
-   textCipher += String.fromCharCode(textChar);
- }
- //retornando el valor
- return textCipher;
-}*/
-
-    /*function mjs(){
-    let mensajeObtenido=mensaje.value;
-    let mayuscula= mensajeObtenido.toUppercase();
-    let mayus= mayuscula.chartCodeAt();
-
-    for(let i=0; i<mayuscula.length; i++){
-      let letraAscii=mayuscula.charCodeAt(i);
-      let formula=(letraAscii-65+offset)%26+65;
-      let resultado= string.formCharcode(formula);
-      nuevoCifrado+=resultado;
-    }
-    document.write(nuevoCifrado);
-  }*/
+/*  x - 65 + n ) % 26 + 65*/
 
   
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 
-//const offset = document.getElementById("offset").value;
+
 const buttonCifrar = document.getElementById("button_encriptar");
 
 buttonCifrar.addEventListener("click", () => {
     
-    const mensaje = document.getElementById("text_plano").value.toUpperCase();
-    const msj_cifrado = cipher.encode (offset,mensaje);
+    const mensaje = document.getElementById("text_plano").value.toUpperCase();// obtener mensaje 
+    const offset = document.getElementById("offset").value;// declare para tomar offfset 
 
-    console.log(msj_cifrado);
-    document.getElementById("mensaje_encriptado").innerHTML= msj_cifrado;
+    const msj_cifrado = window.cipher.encode(offset,mensaje);
+
+    //console.log(msj_cifrado);
+    document.getElementById("mensaje_encriptado").innerHTML= msj_cifrado, offset;
 
 });
 
