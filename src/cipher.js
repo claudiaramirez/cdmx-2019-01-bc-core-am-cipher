acc031fb30be79aa0af9c4d09b42ea1fd6b08069
claudiaramirez <claudiaivonne_15@hotmail.com>
2019-01-17 13:58:55 -0600

    sirve con mensaje encriptado y mostrado en un input

--- src/cipher.js
+++ src/cipher.js
@@ -12,12 +12,27 @@ window.cipher = {
       let resultadoCifrado = String.fromCharCode(conversion);
       cifrado+=resultadoCifrado;
     }
-
     //console.log(cifrado);
     return cifrado 
-  }
-}
+  },
+  /*decode: (offset, mensaje) => {
+    let offsetPasear = parseInt(offset)
+    let cifrado = ""; 
+    for (let i = 0; i < mensaje.length; i++)
+    {
+      let letra = mensaje[i]
+      let obtenerAscii = letra.charCodeAt();
+      let conversion = (obtenerAscii+65-offsetPasear)%26+65;
+      
+      let resultadoCifrado = String.fromCharCode(conversion);
+      cifrado+=resultadoCifrado;
+    }
+    //console.log(cifrado);
+    return cifrado 
 
+
+  }*/
+}
 /*  x - 65 + n ) % 26 + 65*/
 
   
--- src/index.js
+++ src/index.js
@@ -2,15 +2,23 @@
 
 const buttonCifrar = document.getElementById("button_encriptar");
 
-buttonCifrar.addEventListener("click", () => {
-    
+buttonCifrar.addEventListener("click", () => {  
     const mensaje = document.getElementById("text_plano").value.toUpperCase();// obtener mensaje 
     const offset = document.getElementById("offset").value;// declare para tomar offfset 
-
     const msj_cifrado = window.cipher.encode(offset,mensaje);
+    //console.log(msj_cifrado);
+    document.getElementById("mensaje_encriptado").value = msj_cifrado;
+    
+});
 
+/*const buttonDescifrar = document.getElementById("button_desencriptar");
+buttonDescifrar.addEventListener("click", () => {  
+    const mensaje = document.getElementById("text_desencriptar").value.toUpperCase();// obtener mensaje 
+    const offset = document.getElementById("offset").value;// declare para tomar offfset 
+    const msj_cifrado = window.cipher.encode(offset,mensaje);
     //console.log(msj_cifrado);
     document.getElementById("mensaje_encriptado").innerHTML= msj_cifrado, offset;
+});*/
 
-});
+
 
